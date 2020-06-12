c1c6848d857dc8557e9ff2783c8c7b45182ad90c
Alex <alexmillerprojects@gmail.com>
2020-06-12 11:01:05 -0700

    finished sprint

--- names/names.py
+++ names/names.py
@@ -12,11 +12,62 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
+# # Replace the nested for loops below with your improvements
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    def insert(self, value):
+        if value == self.value:
+            # if value not in duplicates:
+            #     duplicates.append(value)
+            self.right = Node(value)
+        if value < self.value:
+            if self.left is None:
+                self.left = Node(value)
+            else:
+                self.left.insert(value)
+        if value > self.value:
+            if self.right is None:
+                self.right = Node(value)
+            else:
+                self.right.insert(value)
+    def contains(self, target):
+        if target == self.value:
+            duplicates.append(self.value)
+        if self.value == None:
+            return False
+        if target > self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+        if self.left is None:
+            return False
+        else:
+            return self.left.contains(target)
+
+# no_dupes = Node("Name")
+
+# for n1 in names_1:
+#     no_dupes.insert(n1)
+# for n2 in names_2:
+#     no_dupes.insert(n2)
+
+# dupes = Node("Jean Velazquez")
+
+# for n1 in names_1:
+#     dupes.insert(n1)
+
+# for x in names_2:
+#     dupes.contains(x)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.storage = []
+        self.capacity = capacity
+
+    class RingBufferFull:
+        def append(self, item):
+            self.storage[self.curr] = item
+            self.curr = (self.curr + 1) % self.capacity
+        def get(self):
+            return self.storage
 
     def append(self, item):
-        pass
+        self.storage.append(item)
+        if len(self.storage) == self.capacity:
+            self.curr = 0
+            self.__class__ = self.RingBufferFull
 
     def get(self):
-        pass
+        return self.storage
